f6fb5daf794beb2ad6dd2a13c6940a4497bdb6c8
Stephen Malone <mail@wirehaiku.org>
2026-06-02 19:46:29 +1000

    Added comments to main script.

--- seska/main.go
+++ seska/main.go
@@ -12,10 +12,12 @@ import (
 	"github.com/stvmln86/seska/seska/tools/dbse"
 )
 
+// Seska is the top-level application controller.
 type Seska struct {
 	List list.List `cmd:"" help:"list existing notes"`
 }
 
+// try fatally prints a non-nil error.
 func try(err error) {
 	if err != nil {
 		fmt.Printf("Error: %s.\n", err.Error())
@@ -23,6 +25,7 @@ func try(err error) {
 	}
 }
 
+// main runs the main Seska program.
 func main() {
 	core := new(Seska)
 	ktxt := kong.Parse(core,
@@ -34,12 +37,13 @@ func main() {
 	path := clui.Path()
 	db, err := dbse.Open(path)
 	try(err)
+	defer try(db.Close())
 
 	tx, err := db.Beginx()
 	try(err)
+	defer try(tx.Commit())
 
 	ktxt.Bind(tx)
 	ktxt.BindTo(os.Stdout, (*io.Writer)(nil))
 	try(ktxt.Run())
-	try(tx.Commit())
 }
